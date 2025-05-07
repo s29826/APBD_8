210468bb03ccf95fdfbd6fbe65c350782210ef43
s29826 <s29826@pjwstk.edu.pl>
2025-05-07 22:33:38 +0200

    Drugi punkt done, błędy na koniec

---
+++ Tutorial8/Controllers/TripsController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial8.Services;
+
+namespace Tutorial8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly ITripsService _tripsService;
+
+        public TripsController(ITripsService tripsService)
+        {
+            _tripsService = tripsService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTrips()
+        {
+            var trips = await _tripsService.GetTrips();
+            return Ok(trips);
+        }
+
+        [HttpGet("/api/clients/{id}/[controller]")]
+        public async Task<IActionResult> GetTrip(int id)
+        {
+            // if( await DoesTripExist(id)){
+            //  return NotFound();
+            // }
+            var trip = await _tripsService.GetTrip(id);
+            if (trip.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(trip);
+        }
+    }
+}
---
+++ Tutorial8/Services/TripsService.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.SqlClient;
+using Tutorial8.Models.DTOs;
+
+namespace Tutorial8.Services;
+
+public class TripsService : ITripsService
+{
+    private readonly string _connectionString = "Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True;Trust Server Certificate=True";
+    
+    public async Task<List<TripDTO>> GetTrips()
+    {
+        var dict = new Dictionary<int, TripDTO>();
+        
+        string command = "SELECT Trip.IdTrip, Trip.Name, Description, DateFrom, DateTo, MaxPeople, C.Name AS C\nFROM Trip\nJOIN s29826.Country_Trip CT on Trip.IdTrip = CT.IdTrip\nJOIN s29826.Country C on CT.IdCountry = C.IdCountry";
+        
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        using (SqlCommand cmd = new SqlCommand(command, conn))
+        {
+            await conn.OpenAsync();
+
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    int idOrdinal = reader.GetInt32(reader.GetOrdinal("IdTrip"));
+                    string country = reader.GetString(reader.GetOrdinal("C"));
+
+                    if (!dict.TryGetValue(idOrdinal, out var dto))
+                    {
+                        dto = new TripDTO()
+                        {
+                            Id = idOrdinal,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            DateFrom = reader.GetDateTime(reader.GetOrdinal("DateFrom")),
+                            DateTo = reader.GetDateTime(reader.GetOrdinal("DateTo")),
+                            MaxPeople = reader.GetInt32(reader.GetOrdinal("MaxPeople")),
+                            Countries = new List<CountryDTO>()
+                        };
+                        dict.Add(idOrdinal, dto);
+                    }
+                    dto.Countries.Add(new CountryDTO {Name = country});
+                }
+
+            }
+
+        }
+        
+
+        return dict.Values.ToList();
+    }
+
+    public async Task<List<TripDTO>> GetTrip(int id)
+    {
+        var dict = new Dictionary<int, TripDTO>();
+
+        string sql = "SELECT Trip.IdTrip, Name, Description, DateFrom, DateTo, MaxPeople, RegisteredAt, PaymentDate\nFROM Trip\nJOIN s29826.Client_Trip CT on Trip.IdTrip = CT.IdTrip\nJOIN s29826.Client C on C.IdClient = CT.IdClient\nWHERE C.IdClient = @id";
+        
+        using (SqlConnection conn = new SqlConnection(_connectionString))
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
+        {
+            cmd.Parameters.AddWithValue("@id", id);
+            await conn.OpenAsync();
+
+            using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    int tripId = reader.GetInt32(reader.GetOrdinal("IdTrip"));
+
+                    if (!dict.TryGetValue(tripId, out var dto))
+                    {
+                        dto = new TripDTO()
+                        {
+                            Id = tripId,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Description = reader.GetString(reader.GetOrdinal("Description")),
+                            DateFrom = reader.GetDateTime(reader.GetOrdinal("DateFrom")),
+                            DateTo = reader.GetDateTime(reader.GetOrdinal("DateTo")),
+                            MaxPeople = reader.GetInt32(reader.GetOrdinal("MaxPeople")),
+                            RegisteredAt = reader.GetInt32(reader.GetOrdinal("RegisteredAt")),
+                            PaymentDate = reader.GetInt32(reader.GetOrdinal("PaymentDate")),
+                        };
+                        dict.Add(tripId, dto);
+                    }
+                }
+            }
+        }
+
+
+        return dict.Values.ToList();
+    }
+}
